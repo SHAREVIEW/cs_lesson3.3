c8ea8a6e71bd720992b3246a6bf8f32e5a6055a5
david <2657070472@qq.com>
2018-10-29 15:08:42 +0800

    switch

--- cs_lesson3.3/Program.cs
+++ cs_lesson3.3/Program.cs
@@ -10,6 +10,29 @@ namespace cs_lesson3._3
     {
         static void Main(string[] args)
         {
+            //switch 语句
+            const string myName = "karli";
+            const string sexyName = "angelina";
+            const string sillyName = "ploppy";
+            string name;
+            Console.WriteLine("What is your name ?");
+            name = Console.ReadLine();
+            switch (name.ToLower())
+            {
+                case myName:
+                    Console.WriteLine("You have the same name as me !");
+                    break;
+                case sexyName:
+                    Console.WriteLine("What a sexy name you have !");
+                    break;
+                case sillyName:
+                    Console.WriteLine("That's a very silly name ! ");
+                    break;
+             }
+            Console.WriteLine("Hi {0}", name);
+            Console.ReadKey();
+
+
         }
     }
 }
